46814ecd5a9912d0f3865b29267149dfba1dbd04
Ubuntu <ubuntu@ip-172-31-39-62.us-east-2.compute.internal>
2024-06-17 07:14:05 +0000

    merge conflict

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,10 @@
-// vite.config.js
-<<<<<<< HEAD
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
->>>>>>> 36d1ebc82457ef49f142418c8e7d2a78dd73a9f1
 
 export default defineConfig({
-    server: {
-        port: 5173,
-<<<<<<< HEAD
-        host: '0.0.0.0', 
-=======
-        host: "0.0.0.0",
->>>>>>> 36d1ebc82457ef49f142418c8e7d2a78dd73a9f1
-    },
-    plugins: [react()],
+	    server: {
+		            port: 5173,
+		            host: "0.0.0.0",
+		        },
+	    plugins: [react()],
 });
-
